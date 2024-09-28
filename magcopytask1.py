d7d32afc0b83436286af6b99b6651501776fc60f
Jonard11 <jonardlagulao15@gmailcom>
2024-09-28 11:25:30 +0800

    akorani

---
+++ magcopytask1.py
@@ -0,0 +1,15 @@
+age = [-12,13,-14,15,-16]
+for data in age: 
+    if data > 0:
+        print(str(data)+ " negative")
+    elif data < 0:
+        print(str(data) +"positive")
+
+
+username = ["juan", "pedro", "james", "sagad", "fuego"]
+
+for datauser in username:
+   if datauser == "james":
+      print ("james cute")
+else:
+    print("james none") 
